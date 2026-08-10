d3324fbe3f19f48e90c8f800930f36d6f2bc2ad3
BLOW241427 <juliogonzalezhincapie@gmail.com>
2026-08-10 07:14:06 -0500

    Add files via upload

---
+++ actividad.py
@@ -0,0 +1,14 @@
+    # 1.calcular area    
+ancho = 5 
+alto = 6 
+
+    # 2.calcular area     
+area = ancho * alto 
+
+    # 3.calcular perimetro 
+perimetro = 2 * (ancho + alto)
+
+
+    # 4. mostrar resulatados
+print("Area:", area)
+print("perimetro:",perimetro)
